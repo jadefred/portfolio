94ea854ceba973615e5d139db637265714b09c50
jadefred <97032726+jadefred@users.noreply.github.com>
2022-08-31 21:29:01 +0200

    Projects - map projects info and image

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,5 +1,15 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import projectsList from "../assets/projects.json";
+
+interface IProjects {
+  id: string;
+  name: string;
+  skills: string;
+  url: string;
+  code: string;
+  image: string;
+}
 
 const Projects: FC = () => {
   const { t } = useTranslation();
@@ -7,6 +17,31 @@ const Projects: FC = () => {
   return (
     <>
       <h2>{t("projects")}</h2>
+      <div>
+        {projectsList &&
+          projectsList.map((project: IProjects) => {
+            return (
+              <div key={project.id} className="grid-cols-1">
+                {/* image */}
+                <div>
+                  <img src={project.image} alt={project.name} />
+                </div>
+
+                {/* projects name and links */}
+                <div>
+                  <div>
+                    <p>{project.name}</p>
+                    <p>{project.skills}</p>
+                  </div>
+                  <div>
+                    <a href={project.code}>Code</a>
+                    {project.url === "" ? "" : <a href={project.url}>Demo</a>}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
